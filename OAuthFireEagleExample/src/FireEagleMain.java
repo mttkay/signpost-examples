428450a4cb9d0d1c90ce6286832befaa6462e2c3
Matthias Käppler <m.kaeppler@gmail.com>
2010-03-12 18:17:51 +0100

    initial commit

---
+++ OAuthFireEagleExample/src/FireEagleMain.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URLEncoder;
+
+import oauth.signpost.OAuth;
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.OAuthProvider;
+import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
+import oauth.signpost.commonshttp.CommonsHttpOAuthProvider;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+public class FireEagleMain {
+
+    public static void main(String[] args) throws Exception {
+
+        OAuthConsumer consumer = new CommonsHttpOAuthConsumer("2qnk0OzpuzzU",
+                "Ctp1QtFbtSaFhOJbOLMCUPio9c75zIaG");
+
+        OAuthProvider provider = new CommonsHttpOAuthProvider(
+                "https://fireeagle.yahooapis.com/oauth/request_token",
+                "https://fireeagle.yahooapis.com/oauth/access_token",
+                "https://fireeagle.yahoo.net/oauth/authorize");
+
+        System.out.println("Fetching request token from Fire Eagle...");
+
+        // we do not support callbacks, thus pass OOB
+        String authUrl = provider.retrieveRequestToken(consumer, OAuth.OUT_OF_BAND);
+
+        System.out.println("Request token: " + consumer.getToken());
+        System.out.println("Token secret: " + consumer.getTokenSecret());
+
+        System.out.println("Now visit:\n" + authUrl + "\n... and grant this app authorization");
+        System.out.println("Enter the verification code and hit ENTER when you're done");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String code = br.readLine();
+
+        System.out.println("Fetching access token from Fire Eagle...");
+
+        provider.retrieveAccessToken(consumer, code);
+
+        System.out.println("Access token: " + consumer.getToken());
+        System.out.println("Token secret: " + consumer.getTokenSecret());
+
+        HttpPost request = new HttpPost("https://fireeagle.yahooapis.com/api/0.1/update");
+        StringEntity body = new StringEntity("city=hamburg&label="
+                + URLEncoder.encode("Send via Signpost!", "UTF-8"));
+        body.setContentType("application/x-www-form-urlencoded");
+        request.setEntity(body);
+
+        consumer.sign(request);
+
+        System.out.println("Sending update request to Fire Eagle...");
+
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpResponse response = httpClient.execute(request);
+
+        System.out.println("Response: " + response.getStatusLine().getStatusCode() + " "
+                + response.getStatusLine().getReasonPhrase());
+    }
+}
---
+++ OAuthTwitterExample/src/TwitterMain.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import oauth.signpost.OAuth;
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.OAuthProvider;
+import oauth.signpost.basic.DefaultOAuthConsumer;
+import oauth.signpost.basic.DefaultOAuthProvider;
+
+public class TwitterMain {
+
+    public static void main(String[] args) throws Exception {
+
+        OAuthConsumer consumer = new DefaultOAuthConsumer("iIlNngv1KdV6XzNYkoLA",
+                "exQ94pBpLXFcyttvLoxU2nrktThrlsj580zjYzmoM");
+
+        OAuthProvider provider = new DefaultOAuthProvider("http://twitter.com/oauth/request_token",
+                "http://twitter.com/oauth/access_token", "http://twitter.com/oauth/authorize");
+
+        System.out.println("Fetching request token from Twitter...");
+
+        // we do not support callbacks, thus pass OOB
+        String authUrl = provider.retrieveRequestToken(consumer, OAuth.OUT_OF_BAND);
+
+        System.out.println("Request token: " + consumer.getToken());
+        System.out.println("Token secret: " + consumer.getTokenSecret());
+
+        System.out.println("Now visit:\n" + authUrl + "\n... and grant this app authorization");
+        System.out.println("Enter the PIN code and hit ENTER when you're done:");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String pin = br.readLine();
+
+        System.out.println("Fetching access token from Twitter...");
+
+        provider.retrieveAccessToken(consumer, pin);
+
+        System.out.println("Access token: " + consumer.getToken());
+        System.out.println("Token secret: " + consumer.getTokenSecret());
+
+        URL url = new URL("http://twitter.com/statuses/mentions.xml");
+        HttpURLConnection request = (HttpURLConnection) url.openConnection();
+
+        consumer.sign(request);
+
+        System.out.println("Sending request to Twitter...");
+        request.connect();
+
+        System.out.println("Response: " + request.getResponseCode() + " "
+                + request.getResponseMessage());
+    }
+}
